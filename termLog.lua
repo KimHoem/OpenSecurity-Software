285c5cdc46b63663fd3c27e7065ba1ea61762c86
KimHoem <30677184+KimHoem@users.noreply.github.com>
2017-08-03 20:06:34 +0200

    Created function log() in termLog.lua
    function log(level, msg)
    level: level of urgency (info, debug, warning, critical)
    msg: the passed on message to display in the terminal

---
+++ termLog.lua
@@ -0,0 +1,13 @@
+term = require("term")
+
+function log(level, msg)
+    if level = 1 then term.write("[INFO]")
+    elseif level = 2 then term.write("[DEBUG]")
+    elseif level = 3 then term.write("[WARNING]")
+    elseif level = 4 then term.write("[CRITICAL]")
+    else
+        term.write("[ ? ]")
+    end
+
+    term.write(msg .. "\n")
+end
