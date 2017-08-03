9bff4d86646b6a6f7cb96d456ead9b7402de8374
KimHoem <30677184+KimHoem@users.noreply.github.com>
2017-08-03 20:10:28 +0200

    Added missing equals
    The if statements had only single equals. Updated to double ecuals.

--- termLog.lua
+++ termLog.lua
@@ -1,10 +1,10 @@
 term = require("term")
 
 function log(level, msg)
-    if level = 1 then term.write("[INFO]")
-    elseif level = 2 then term.write("[DEBUG]")
-    elseif level = 3 then term.write("[WARNING]")
-    elseif level = 4 then term.write("[CRITICAL]")
+    if level == 1 then term.write("[INFO]")
+    elseif level == 2 then term.write("[DEBUG]")
+    elseif level == 3 then term.write("[WARNING]")
+    elseif level == 4 then term.write("[CRITICAL]")
     else
         term.write("[ ? ]")
     end
